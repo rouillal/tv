bf8660e86e4e2f526705b5fc0ce72d82d53a15de
cataniaj <j.catania.c@gmail.com>
2017-02-07 10:29:14 +0100

    td3'

--- TD4-OpenJML-2017/src/Explosives.java
+++ TD4-OpenJML-2017/src/Explosives.java
@@ -6,30 +6,30 @@ public class Explosives{
     public int nb_assign = 0;
     public String [][] assign = new String[30][2];
  
-    /*@ public invariant // Prop 1
-      @ (0 <= nb_inc && nb_inc < 50);
+    /*@ public invariant // Prop 1 : le nombre de produits incompatibles dans un bâtiment est compris entre 0 et 49
+      @ (0 <= nb_inc && nb_inc < 50); 
       @*/
-    /*@ public invariant // Prop 2
+    /*@ public invariant // Prop 2 : le nombre de produits assignés dans un bâtiment est compris entre 0 et 29
       @ (0 <= nb_assign && nb_assign < 30);
       @*/
-    /*@ public invariant // Prop 3
+    /*@ public invariant // Prop 3 : chaque produit incompatible dans un bpatiment doit commencer par la chaine de caractère 'Prod'
       @ (\forall int i; 0 <= i && i < nb_inc; 
       @         incomp[i][0].startsWith("Prod") && incomp[i][1].startsWith("Prod"));
       @*/
-    /*@ public invariant // Prop 4
+    /*@ public invariant // Prop 4 : chaque produit assigné à un bâtiment doit commencer par 'Prod' et chaque bâtiment doit commencer par 'Bat' 
       @ (\forall int i; 0 <= i && i < nb_assign; 
       @         assign[i][0].startsWith("Bat") && assign[i][1].startsWith("Prod"));
       @*/
-    /*@ public invariant // Prop 5
+    /*@ public invariant // Prop 5 : un produit ne peut pas être incompatible avec lui-même
       @ (\forall int i; 0 <= i && i < nb_inc; !(incomp[i][0]).equals(incomp[i][1]));
       @*/
-    /*@ public invariant // Prop 6
+    /*@ public invariant // Prop 6 : Si un produit A est incompatible avec un produit B, alors B est incompatible avec A
       @ (\forall int i; 0 <= i && i < nb_inc; 
       @        (\exists int j; 0 <= j && j < nb_inc; 
       @           (incomp[i][0]).equals(incomp[j][1]) 
       @              && (incomp[j][0]).equals(incomp[i][1]))); 
       @*/
-    /*@ public invariant // Prop 7
+    /*@ public invariant // Prop 7 : si un produit A est assigné 
       @ (\forall int i; 0 <= i &&  i < nb_assign; 
       @     (\forall int j; 0 <= j && j < nb_assign; 
       @        (i != j && (assign[i][0]).equals(assign [j][0])) ==>

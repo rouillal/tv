a84bc0a78eaa59de859e327b31abdef1156ee502
Lorrie Rouillaux <lorrie.rouillaux@gmail.com>
2017-03-01 15:20:45 +0100

    Theory pour ComputeScoreTestWithTheories.java ne marchent pas

--- TAT/hellomaven/src/test/java/com/example/hellomaven/ComputeScoreTestWithTheories.java
+++ TAT/hellomaven/src/test/java/com/example/hellomaven/ComputeScoreTestWithTheories.java
@@ -2,6 +2,7 @@ package com.example.hellomaven;
 
 import static org.junit.Assert.assertTrue;
 
+import org.junit.Assert;
 import org.junit.Assume;
 import org.junit.contrib.theories.DataPoints;
 import org.junit.contrib.theories.Theories;
@@ -11,19 +12,30 @@ import org.junit.runner.RunWith;
 
 @RunWith(Theories.class)
 public class ComputeScoreTestWithTheories {
-    @DataPoints
-    public static int[] scores() {
-        return new int[]{
-            10, 7, 2,10,0,0,6,4,3,3,10,0,1,10,5,4};
-    }
-    @Theory
-    public void a_plus_b_is_greater_than_a_and_greater_than_b(Integer a, Integer b) {
-        Assume.assumeTrue(a >0 && b > 0 );
-        assertTrue(a + b > a);
-        assertTrue(a + b > b);
-    }
-    @Theory
-    public void addition_is_commutative(Integer a, Integer b) {
-        assertTrue(a + b == b + a);
-    }
+	@DataPoints
+	public static int[][] carreaux() {
+		return new int[][]{{},{2},{2,7},{3,6,2,5},{3,7,2,5},{10,2,5},{10, 0, 0, 2}};
+	}
+	@Theory
+	public void verify(int []carreau) {
+		Assume.assumeTrue(carreau.length <= 10*2); //moins de 10 carreaux à 2 lancers
+		for (int i = 0; i <carreau.length; i++)
+			Assume.assumeTrue(carreau[i] >= 0 && carreau[i] <= 10); //chaque lancer est compris entre 0 et 10
+		assertTrue(ComputeScore.computeScore(carreau) >= 0); //score positif
+		assertTrue(ComputeScore.computeScore(carreau) >= scoreMin(carreau));//score au moins supérieur à la somme de tous les lancers
+	}
+	
+	@Theory
+	   public void testBlankArrays(int[] carreau)
+	   {
+	      Assume.assumeTrue(carreau.length == 0);
+	      Assert.assertEquals(ComputeScore.computeScore(carreau),0);
+	   }
+	
+	public int scoreMin(int []carreau){
+		int score = 0;
+		for(int i = 0; i<carreau.length; i++)
+			score += carreau[i];
+		return score;
+	}
 }
